2f9e43f008b00569443e3908e2b5ec2914500179
Naushad.Alam <Naushad.Alam@MW-LT-076.mavenwave.local>
2020-06-09 10:09:14 +0530

    First commit

---
+++ elc-automation/src/main/java/com/estee/automation/elc_automation/App.java
@@ -0,0 +1,13 @@
+package com.estee.automation.elc_automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
